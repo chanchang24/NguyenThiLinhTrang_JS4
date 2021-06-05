17c0b2b2a861596182216a2f8b2d93845f21c5a5
chanchang24 <83943885+chanchang24@users.noreply.github.com>
2021-06-06 01:28:16 +0700

    update

--- main.js
+++ main.js
@@ -13,46 +13,88 @@ btnSapXep.onclick = function () {
     var b = document.getElementById("soB").value;
     var c = document.getElementById("soC").value;
 
-    if (a < b && a < c) {
-        if (c < b) {
-            kq.innerHTML = a + ' ' + c + ' ' + b;
+    if (a !== parseInt(a) && b !== parseInt(b) && c !== parseInt(c)) {
+        alert('Vui lòng nhập số nguyên');
+    } else {
+        if (a < b && a < c) {
+            if (c < b) {
+                kq.innerHTML = a + ' ' + c + ' ' + b;
+            }
+            kq.innerHTML = a + ' ' + b + ' ' + c;
+        } else if (b < a && b < c) {
+            if (c < a) {
+                kq.innerHTML = b + ' ' + c + ' ' + a;
+            }
+            kq.innerHTML = b + ' ' + a + ' ' + c;
+        } else {
+            if (a < b) {
+                kq.innerHTML = c + ' ' + a + ' ' + b;
+            }
+            kq.innerHTML = c + ' ' + b + ' ' + a;
         }
-        kq.innerHTML = a + ' ' + b + ' ' + c;
-    }else if (b < a && b < c){
-        if (c < a){
-            kq.innerHTML = b + ' ' + c + ' ' + a;
-        }
-        kq.innerHTML = b + ' ' + a + ' ' + c;
-    }else{
-        if(a < b ){
-            kq.innerHTML = c + ' ' + a + ' ' + b;
-        }
-        kq.innerHTML = c + ' ' + b + ' ' + a;
     }
+
 }
 /*Bài 2: chương trình “Chào hỏi” các thành viên trong gia đình
 B1: Input
 - nhập ký tự Bố (B), Mẹ (M), anh Trai (A) và Em gái (E)
 B2:Handle
 - tạo biến kyTu
-- chia ra 3 trường hợp:,so sánh lần lượt 2 số với 1 số, tìm ra số bé nhất, tiếp tục so sánh 2 số còn lại với nhau.
+- So sánh ký tự nhập vào với biến kyTu
 B3: Output
 - Xuất lời chào
 */
 var btnGuiLoiChao = document.getElementById('btnGuiLoiChao');
-btnGuiLoiChao.onclick = function(){
+btnGuiLoiChao.onclick = function () {
     var kyTu = document.getElementById('nguoiSD').value;
-    if(kyTu === 'B'){
+    if (kyTu === 'B') {
         loiChao.innerHTML = 'Xin chào bố';
-    }else if(kyTu === 'M'){
+    } else if (kyTu === 'M') {
         loiChao.innerHTML = 'Xin chào mẹ';
-    }else if(kyTu === 'A'){
+    } else if (kyTu === 'A') {
         loiChao.innerHTML = 'Xin chào anh';
-    }else if(kyTu === 'E'){
-        loiChao.innerHTML = 'Xin chào anh';
-    }else{
-        if(kyTu !== 'B' || kyTu !== 'M' || kyTu !== 'A' || kyTu !== 'E' )
-        alert('Vui lòng nhập lại ký tự')
+    } else if (kyTu === 'E') {
+        loiChao.innerHTML = 'Xin chào em';
+    } else {
+        if (kyTu !== 'B' || kyTu !== 'M' || kyTu !== 'A' || kyTu !== 'E')
+            alert('Vui lòng nhập lại ký tự')
     }
-
 }
+/*Bài 3: chương trình xuất ra bao nhiêu số chẵn số lẻ
+B1: Input
+- nhập giá trị 3 số nguyên
+B2:Handle
+- tạo biến soThuNhat, soThuHai, soThuBa
+- 
+B3: Output
+- 
+*/
+
+var btnXuat = document.getElementById('btnXuat');
+btnXuat.onclick = function () {
+    var soThuNhat = document.getElementById('soThuNhat').value;
+    var soThuHai = document.getElementById('soThuHai').value;
+    var soThuBa = document.getElementById('soThuBa').value;
+
+    if (soThuNhat % 2 === 0 && soThuHai % 2 === 0 && soThuBa % 2 === 0) {
+        alert('Có 3 số chẵn và 0 số lẻ')
+    } else if (soThuNhat % 2 === 0 && soThuHai % 2 === 0 && soThuBa % 2 !== 0) {
+        alert('Có 2 số chẵn và 1 số lẻ')
+    } else if (soThuNhat % 2 === 0 && soThuHai % 2 !== 0 && soThuBa % 2 !== 0) {
+        alert('Có 1 số chẵn và 2 số lẻ')
+    } else if (soThuNhat % 2 === 0 && soThuHai % 2 !== 0 && soThuBa % 2 === 0) {
+        alert('Có 2 số chẵn và 1 số lẻ')
+    } else if (soThuNhat % 2 === 0 && soThuHai % 2 === 0 && soThuBa % 2 !== 0) {
+        alert('Có 2 số chẵn và 1 số lẻ')
+    } else if (soThuNhat % 2 !== 0 && soThuHai % 2 === 0 && soThuBa % 2 === 0) {
+        alert('Có 2 số chẵn và 1 số lẻ')
+    } else if (soThuNhat % 2 !== 0 && soThuHai % 2 !== 0 && soThuBa % 2 === 0) {
+        alert('Có 1 số chẵn và 2 số lẻ')
+    } else if (soThuNhat % 2 !== 0 && soThuHai % 2 === 0 && soThuBa % 2 !== 0) {
+        alert('Có 1 số chẵn và 2 số lẻ')
+    } else {
+        alert('Có 0 số chẵn và 3 số lẻ')
+    }
+}
+
+
